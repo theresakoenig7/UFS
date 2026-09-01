5e82f3c2f3ca34caa19dc745f27d9dd44f20be01
theresakoenig7 <theresa.koenig.7@googlemail.com>
2026-09-01 11:18:49 +0200

    added times

--- exploration.py
+++ exploration.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 # Unterordner mit den CSV-Dateien
 input_dir = Path("03_LueckenloseDaten")
@@ -14,13 +15,19 @@ for csv_path in input_dir.glob("*.csv"):
     df = pd.read_csv(csv_path, parse_dates=["Zulu"])
     df = df.set_index("Zulu")
 
-    plt.figure(figsize=(10, 5))
-    plt.plot(df.index, df["Temp"])
-    plt.xlabel("Datum")
-    plt.ylabel("Wert")
-    plt.title(csv_path.stem)
+    fig, ax = plt.subplots(figsize=(20, 5))
+    ax.plot(df.index, df["Temp"])
+
+    # Tick fuer jede Minute
+    ax.xaxis.set_major_locator(mdates.MinuteLocator(interval=5))
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
+    plt.xticks(rotation=90, fontsize=7)
+
+    ax.set_xlabel("Datum")
+    ax.set_ylabel("Wert")
+    ax.set_title(csv_path.stem)
     plt.tight_layout()
 
     output_path = output_dir / f"{csv_path.stem}.png"
     plt.savefig(output_path)
-    plt.show()
+    plt.close()
